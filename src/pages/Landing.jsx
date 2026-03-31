f4d9b6b794986d8e0cf4887e5cd64409be9abb64
Vipashyanawagh <vipashyana.wagh2023@sait.ac.in>
2026-03-31 21:25:17 +0530

    FirstCommit

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Airplay } from 'lucide-react'
 import Navbar from '../components/landing/Navbar'
-import Cardbox from '../components/landing/Cardbox'
 import StockCard from '../components/landing/StockCard'
 import CardPlate from '../components/landing/CardPlate'
 import CardPlate1 from '../components/landing/CardPlate1'
 import Footer1 from '../components/landing/Footer1'
 import Footer from '../components/landing/Footer'
-
+import Cardbox from '../components/landing/Cardbox'
 function Landing() {
   return (
     <div>
@@ -15,7 +14,7 @@ function Landing() {
       <Navbar />
      
      <StockCard/>
-     <Cardbox/>
+     <Cardbox />
      <CardPlate/>
      <CardPlate1/>
      <Footer/>
